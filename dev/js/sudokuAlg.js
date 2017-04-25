c1f40513c49921d26c2e684f56af327a92752fae
bojan-dim <boyan.idimitrov@gmail.com>
2017-04-25 11:19:29 +0300

    Update sudokuAlg.js

--- dev/js/sudokuAlg.js
+++ dev/js/sudokuAlg.js
@@ -22,23 +22,70 @@ for( var elementCount = 0 ; elementCount < 8 ; elementCount++) {
         sorted = 0;
         playElementValue = Math.floor((Math.random() * 4) + 1);
 
-
+        // check if the randow generated number is NOT in the row already
         for ( row = rowPosit, col = 0 ; col < 4 ; col++) {
             if( playTable[row][col] == playElementValue && row != rowPosit && col != colPosit ) {
                 sorted = 1;
             }
+            continue;
         }
 
+        // check if the randow generated number is NOT in the col already
         for ( col = colPosit, row = 0 ; row < 4 ; row++) {
             if( playTable[row][col] == playElementValue && row != rowPosit && col != colPosit) {
                 sorted = 1;
             }
+            continue;
         }
 
-        for ( )
+        //4 checks for each 2x2 block in the 4x4 playtable - check if the number is already there
+        if( rowPosit <= 1 && colPosit <=1 ) {
+            for ( row = 0 ; row < 2 ; row++) {
+                for( col = 0 ; col < 2 ; col++ ) {
+                    if (playTable[row][col] == playElementValue && row != rowPosit && col != colPosit) {
+                        sorted = 1;
+                    }
+                    continue;
+                }
+            }
+        }
+
+        if( rowPosit <= 1 && colPosit <= 3 ) {
+            for ( row = 0 ; row < 2 ; row++) {
+                for( col = 2 ; col < 4 ; col++ ) {
+                    if (playTable[row][col] == playElementValue && row != rowPosit && col != colPosit) {
+                        sorted = 1;
+                    }
+                    continue;
+                }
+            }
+        }
+
+        if( rowPosit <= 3 && colPosit <= 1 ) {
+            for ( row = 2 ; row < 4 ; row++) {
+                for( col = 0 ; col < 2 ; col++ ) {
+                    if (playTable[row][col] == playElementValue && row != rowPosit && col != colPosit) {
+                        sorted = 1;
+                    }
+                    continue;
+                }
+            }
+        }
+
+        if( rowPosit <= 3 && colPosit <= 3 ) {
+            for ( row = 2 ; row < 4 ; row++) {
+                for( col = 2 ; col < 4 ; col++ ) {
+                    if (playTable[row][col] == playElementValue && row != rowPosit && col != colPosit) {
+                        sorted = 1;
+                    }
+                    continue;
+                }
+            }
+        }
 
     } while( sorted = 1 );
 
     playTable[rowPosit][colPosit] = playElementValue;
-
 }
+
+
