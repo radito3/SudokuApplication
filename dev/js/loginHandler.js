b7b52d5a716d18b008152982fc1b9cb7358e41c8
Rangel Ivanov <radito3@mail.bg>
2017-05-29 21:45:00 +0300

    More changes
    Continuing final changes

--- dev/js/loginHandler.js
+++ dev/js/loginHandler.js
@@ -1,28 +1,27 @@
 var Users = [];
 var one = {firstName:"Rangel", lastName:"Ivanov", username:"radito3", password:"12345"};
-var two = {firstName:"Boyan", lastName:"Dimitov", username:"bobi24"};
-var three = {firstName:"Lachezar", lastName:"Zahariev", username:"lycezar"};
+var two = {firstName:"Boyan", lastName:"Dimitov", username:"bobi24", password:"12345"};
+var three = {firstName:"Lachezar", lastName:"Zahariev", username:"lycezar", password:"12345"};
 Users.push(one); Users.push(two); Users.push(three);
 
-$("#submit").click(function () {
+$(document).ready(function() {
+	//$("#User-Name-Placeholder").val("Radito3");
+	$("#isubmit").click(function () {
+		$("#UserName").css("visibility", "hidden");
+		$("#ipassword").css("visibility", "hidden");
+	}
+})
+
+$("#isubmit").click(function () {
+	window.location.replace("home.html");
 	for (var i = 0; i < Users.length; i++) {
-		if (Users[i].username == $("#userName").val() && Users[i].password == $("#ipassword").val()) {
-			window.location.replace("home.html");
+		if (Users[i].username == $("#UserName").val() && Users[i].password == $("#ipassword").val()) {
+
 		} else {
 			alert("Incorrect username or password!");
-			$("#userName").text("");
+			$("#UserName").text("");
 			$("#ipassword").text("");
 		}
 	}
 })
 
-
-
-/*if (typeof(Storage) !== "undefined") {
-    // Store
-    localStorage.setItem("lastname", "Smith");
-    // Retrieve
-    $("result").text(localStorage.getItem("lastname"));
-} else {
-    $("result").text("Sorry, your browser does not support Web Storage...");
-}*/
